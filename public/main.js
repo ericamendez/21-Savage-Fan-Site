d57ff8c8fb1461cbcee1eaf1668298f3a2051ff0
ericamendez <ecmendez25@gmail.com>
2018-11-15 16:09:39 -0500

    complete and working

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
 var trash = document.getElementsByClassName("fa-trash");
 
 Array.from(thumbUp).forEach(function(element) {
@@ -19,6 +20,32 @@ Array.from(thumbUp).forEach(function(element) {
           if (response.ok) return response.json()
         })
         .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(thumbDown).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[9].innerText)
+        console.log(thumbDown)
+        fetch('messagesDown', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbDown':thumbDown
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+
           console.log(data)
           window.location.reload(true)
         })
